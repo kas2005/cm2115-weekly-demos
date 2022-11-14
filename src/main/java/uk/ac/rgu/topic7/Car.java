df0ed208c9669a9077b5b4994eda1ebd7311aa2d
Katherine Lavery <k.lavery@rgu.ac.uk>
2022-11-14 11:03:34 +0000

    deleted

--- src/main/java/uk/ac/rgu/topic7/Car.java
+++
@@ -1,7 +0,0 @@
-package uk.ac.rgu.topic7;
-
-public interface Car {
-
-    public void name();
-    
-}
--- src/main/java/uk/ac/rgu/topic7/CarVisitor.java
+++
@@ -1,8 +0,0 @@
-package uk.ac.rgu.topic7;
-
-public interface CarVisitor {
-
-    public void visit(Ford car);
-    
-    public void visit(Ferrari Car);
-}
